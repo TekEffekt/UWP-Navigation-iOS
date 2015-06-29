ebfdc5e1ee9a12dc67c07a8bff409682cead6134
Kyle Zawacki <Kyle@admiins-MacBook-Pro.local>
2015-06-29 17:46:24 -0500

    Initial Commit

---
+++ UWP-Navigation-iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UWP-Navigation-iOS
+//
+//  Created by Kyle Zawacki on 6/29/15.
+//  Copyright © 2015 App Factory. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
